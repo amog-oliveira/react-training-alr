3b9a44cc92d6ea048338186d000daab4e365a671
Amanda de Oliveira <amandam.oliveirag@gmail.com>
2021-08-30 20:01:33 -0300

    create class composition)

--- index.js
+++ index.js
@@ -12,12 +12,14 @@ cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.depositar(500);
 
-contaCorrenteRicardo.depositar(-100);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 102;
+console.log(conta2);
 
-const valorSacado = contaCorrenteRicardo.sacar(75);
-console.log(valorSacado);
-
+contaCorrenteRicardo.transferir(200, conta2);
+console.log(conta2);
 console.log(contaCorrenteRicardo);
