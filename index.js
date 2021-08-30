9e54adda91dcfd9616a54eb448387e3d54a3f5b0
Amanda de Oliveira <amandam.oliveirag@gmail.com>
2021-08-30 17:40:49 -0300

    create classes behaviors

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ class Cliente{
   nome;
   cpf;
 }
+
 class ContaCorrente{
   agencia;
   saldo;
+
+  sacar(valor) {
+    if (this.saldo >= valor) {
+      this.saldo -= valor;
+    }
+  }
 }
 
 const cliente1 = new Cliente();
@@ -15,7 +22,16 @@ const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
-const contaCorrenteRicardo = new ContaCorrente();
 
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo = 100;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.sacar(75);
+
+console.log(contaCorrenteRicardo.saldo);
 console.log(cliente1);
 console.log(cliente2);
